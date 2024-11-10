fab604ca7f9468217bec7c07df357613327ae380
Zion Vartanian <zionvartanian57@gmail.com>
2024-11-10 11:50:44 -0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+st.set_page_config(page_title='Welcome to Zion Transactions', 
+                   page_icon="👋",
+                   )
+
+st.title("Welcome to Zion Risk Reviews")
+
+st.markdown(
+    """
+    These 2 apps help review possible fraud
+
+    👈 Select an app from the sidebar to start
+
+    If the app doesn't work contact Zion Vartanian
+
+    Want to learn more?
+    - Check out my [LinkedIn](https://www.linkedin.com/in/zion-vartanian/)
+
+    """
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
